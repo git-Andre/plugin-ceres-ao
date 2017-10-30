e8bf5a3d4bc2379531764f0ca058dd53460868c8
git-Andre <post@andre-oelschlaegel.de>
2017-10-30 18:24:30 +0100

    isAuction ? in CategoryItem

--- resources/js/src/app/components/itemList/CategoryItem.js
+++ resources/js/src/app/components/itemList/CategoryItem.js
@@ -1,43 +1,50 @@
-Vue.component("category-item", {
+Vue.component( "category-item", {
 
     template: "#vue-category-item",
 
     props: [
         "decimalCount",
         "itemData",
-        "imageUrlAccessor"
+        "imageUrlAccessor",
+        "isAuction"
     ],
 
-    data: function()
-    {
+    data: function () {
         return {
             recommendedRetailPrice: 0,
-            variationRetailPrice  : 0
+            variationRetailPrice: 0
         };
     },
 
-    created: function()
-    {
+    created: function () {
         this.recommendedRetailPrice = this.itemData.calculatedPrices.rrp.price;
-        this.variationRetailPrice = this.itemData.calculatedPrices.default.price;
+        this.variationRetailPrice   = this.itemData.calculatedPrices.default.price;
     },
 
     computed:
-    {
-        /**
-         * returns itemData.item.storeSpecial
-         */
-        storeSpecial: function()
         {
-            return this.itemData.item.storeSpecial;
-        },
+            /**
+             * returns itemData.item.storeSpecial
+             */
+            storeSpecial: function () {
+                return this.itemData.item.storeSpecial;
+            },
 
-        /**
-         * returns itemData.texts[0]
-         */
-        texts: function()
-        {
-            return this.itemData.texts;
+            /**
+             * returns itemData.texts[0]
+             */
+            texts: function () {
+                return this.itemData.texts;
+            },
+            isAuction: () => {
+                // return true if Auktion in property (Facets)
+                for (var i = this.itemData.properties.length; --i >= 0;) {
+                    // todo: config prop-names
+                    if ( this.itemData.properties[i].names.name == "Auktion" || this.itemData.properties[i].nams.name == "auction" ) {
+                        return true;
+                    }
+                }
+                return false;
+            }
         }
-    }
-});
+} );
